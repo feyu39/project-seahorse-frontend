2d64fccb3f8a72a012b9b141993dcd74e55fed66
Felix Yu <felix.yu@stryker.com>
2023-07-07 12:15:08 -0400

    Added routes to router v2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import UserRequest from '../views/UserRequest.vue'
 
 const routes = [
   {
@@ -8,13 +9,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/send',
+    name: 'sendItem',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "sendItem" */ '../views/SendingItems.vue')
+  },
+  {
+    path: '/request',
+    name: 'requestItem',
+    component: UserRequest
   }
+
 ]
 
 const router = createRouter({
--- src/views/UserRequest.vue
+++ src/views/UserRequest.vue
@@ -1,6 +1,11 @@
 <template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
-
+  
 <script setup>
 
 </script>
+  
