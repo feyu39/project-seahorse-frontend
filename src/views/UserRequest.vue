856650261a0a361e213756023bf19cabb96bebd3
Felix Yu <felix.yu@stryker.com>
2023-07-14 15:24:14 -0400

    Installed axios, updated router view

--- src/views/UserRequest.vue
+++ src/views/UserRequest.vue
@@ -1,8 +1,4 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
-
   <form @submit.prevent="submitRequest">
     <!-- Delivery location (needed for popup screen, Doug can edit later) -->
     <label for="delivery-location">Delivery Location:</label>
